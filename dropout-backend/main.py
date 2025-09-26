84499e841a5695050881c2249f83d7152bc2f1d3
Lakshhhhh15 <snortsniper1004@gmail.com>
2025-09-26 06:46:26 +0000

    Fix import path for database module in main.pyupdate

--- dropout-backend/main.py
+++ dropout-backend/main.py
@@ -6,7 +6,7 @@ from mangum import Mangum
 import pickle
 import numpy as np
 
-from database import SessionLocal, engine, Base
+from dropout-backend.database import SessionLocal, engine, Base
 import models, schemas
 
 # ✅ Initialize app only once
